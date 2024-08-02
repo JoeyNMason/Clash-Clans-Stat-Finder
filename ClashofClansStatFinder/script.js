9546a6f8dfe335e79e41ae2fbfe29d57606d35cb
Joey <160741266+JoeyNMason@users.noreply.github.com>
2024-08-02 02:23:02 +0100

    Add files via upload

--- ClashofClansStatFinder/script.js
+++ ClashofClansStatFinder/script.js
@@ -1,9 +1,45 @@
 // bring in the search button and give functionality
+document.getElementById('search-btn').addEventListener('click', () => {
+    const playerStats = document.getElementById('tag').value;
+    if (playerStats){
+        getStats(playerStats)
+    }
+});
 
-// bring in the api key and the api url and maybe the token?? 
-// token might need to be in a different file
+// bring in the api url and token 
+function getStats(playerStats){
+    const token = "";
+    const apiURL = `https://api.clashofclans.com/v1/players/%23${playerStats}`;
+    // %23 is url encoding for #
 
-// fetch and catch errors
+    // fetch and catch errors
+    fetch(apiURL , {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+    .then(response =>{
+        if(!response.ok){
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        displayStats(data);
+    })
+    .catch(error => {
+        console.error('Error fetching player stat data:', error);
+        alert('Stats not found');
+    })
+};
+
+// function to display stats in console for now
+function displayStats(data){
+    console.log(data);
+}
+
+
 
 // bring in the ids
 // change text content
