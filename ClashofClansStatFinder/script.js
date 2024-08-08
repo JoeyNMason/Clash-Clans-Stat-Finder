288bf2dc8ca27d6218d22be625085ad60395a0a4
Joey <160741266+JoeyNMason@users.noreply.github.com>
2024-08-08 12:47:34 +0100

    Add files via upload

--- ClashofClansStatFinder/script.js
+++ ClashofClansStatFinder/script.js
@@ -8,9 +8,14 @@ document.getElementById('search-btn').addEventListener('click', () => {
 
 // bring in the api url and token 
 function getStats(playerStats){
-    const token = "";
-    const apiURL = `https://api.clashofclans.com/v1/players/%23${playerStats}`;
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjcyMzc5YWViLTkxZTUtNGY3My1iMmQ3LTdiM2YyMDM0ZTAwNiIsImlhdCI6MTcyMzExMzkxMiwic3ViIjoiZGV2ZWxvcGVyL2Q5YTkzZmYyLWU4MWItMTg1My1mYjk4LWU1OGRmYTUzZTJiMyIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.X-uuA5zcXKIC5wZzlWGpTLxT5pzvXhoFNTENTWELR08g-0lHn_7bRdXCzjdjEwxP4RVOlfR59x5nXKylmPp_8g";
+    const apiURL = `https://cocproxy.royaleapi.dev/v1/players/%23${playerStats}`;
+    // https://api.clashofclans.com/v1/players/%23${playerStats}
+    // https://cocproxy.royaleapi.dev
+    // https://cocproxy.royaleapi.dev/v1/players/%23${playerStats}
     // %23 is url encoding for #
+
+    console.log(`Requesting data from: ${apiURL}`);
 
     // fetch and catch errors
     fetch(apiURL , {
@@ -38,8 +43,6 @@ function getStats(playerStats){
 function displayStats(data){
     console.log(data);
 }
-
-
 
 // bring in the ids
 // change text content
